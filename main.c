86427c6489af9e98b7af1975bde088fac4dd03f4
Suman Sahoo <ssamsomu@gmail.com>
2022-03-11 18:45:01 +0530

    develop the outputs

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <conio.h>
+#include <time.h>
 
 void start();
 
@@ -9,11 +11,11 @@ void showMenu();
 bool Valid();
 
 int main(){
-    
+
 
     start();
 
-    
+
     return 0;
 }
 
@@ -24,14 +26,36 @@ void start(){
     printf("                                                       BUS TICKET RESERVATION SYSTEM\n                                                       Made by SHREEYA, HEMANT, SUMAN\n                                                        IIIT BHUBANESHWAR\n");
     printf("----------------------------------------------------------------------------------------------------------------------------------------------------------\n");
     printf("---------------------------------------------------------PRESS ANY KEY TO CONTINUE-------------------------------------------------------------\n");
+    getch();
     printf("----------------------------------------------------------------------------------------------------------------------------------------------------------\n");
 
-    
 
     bool UserNameValid = Valid();
+    if(UserNameValid==true){
+        printf("\n");
+        printf("----------------------------------------LOGIN IS SUCCESSFUL-------------------------------------------------\n");
+        //printf("==============================WELCOME TO SHS BUS RESERVATION SYSTEM!!!======================================\n");
+        //printf("------------------------------------------------------------------------------------------------------------\n");
+        printf("Press any key to continue...\n");
+        getch();
 
-    showMenu();
-    
+        printf("LOADING");
+        Sleep(1000);
+        printf(".");
+        Sleep(1000);
+        printf(".");
+        Sleep(1000);
+        printf(".");
+        Sleep(1000);
+
+
+        showMenu();
+
+    }
+    else{
+        printf("WRONG CREDENTIALS");
+    }
+
 
 
 }
@@ -42,21 +66,34 @@ bool Valid(){
     char pass[30];
     char name[30];
     printf("-----------------------------------------------LOGIN--------------------------------------------------------\n");
-    printf("                    ENTER USERNAME :- ");
+    printf("                    ENTER USERNAME :- ");
     gets(name);
-    printf("\n");
-    printf("                    ENTER PASSWORD :- ");
-    gets(pass);
-    printf("\n");
-    printf("----------------------------------------LOGIN IS SUCCESSFUL-------------------------------------------------\n");
-    printf("==============================WELCOME TO SHS BUS RESERVATION SYSTEM!!!======================================\n");
-    printf("------------------------------------------------------------------------------------------------------------\n");
-    printf("Press any key to continue...\n");
+
+    char nCheck[]={"suman"};
+    char pCheck[]={"sahoo"};
+    if(  strcmp(name, nCheck) ==0)
+    {
+        printf("\n");
+        printf("                    ENTER PASSWORD :- ");
+        gets(pass);
+
+        if(strcmp(pass,pCheck)==0){
+                return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    else{
+        return false;
+    }
+
 
 
 }
 void showMenu(){
-    printf("WELCOME TO SHS BUS RESERVATION SYSTEM");
+    printf("\n\nWELCOME TO SHS BUS RESERVATION SYSTEM");
     printf("1.\tView all available bus\n2.\tBook tickets\n3.\tCancel tickets\n4.\tView book tickets\n5.\tExit");
 }
 
