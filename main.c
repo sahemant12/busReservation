0af6edcaa638930f48f4de83766668512bef0b84
sahemant12 <96417076+sahemant12@users.noreply.github.com>
2022-03-12 11:35:42 +0530

    Update main.c

--- main.c
+++ main.c
@@ -146,3 +146,18 @@ void availableBus(){
 
 }
 
+bookTickets(){
+        int num;
+    char em[20] = {"Empty"};
+    printf("========BUS RESERVATION SYSTEM==========\n");
+    printf("Enter Bus no. :- ");
+    scanf("%d",&num);
+    printf("\n");
+    int number =1;
+    for(int i=1; i<=6; i++){
+        for(int j=1; j<=4; j++){
+        printf("%d.%s    ",number,em);
+        number++;
+        }
+        printf("\n");
+}
