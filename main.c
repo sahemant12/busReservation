f62542b9f50cba3e8481593bc112740599b19fe2
sahemant12 <96417076+sahemant12@users.noreply.github.com>
2022-03-12 10:49:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -131,17 +131,17 @@ void showMenu(){
 void availableBus(){
 
     printf("--------------------------------------SHS BUS RESERVATION SYSTEM--------------------------------------------\n\n");
-    printf("Bus No.          Bus Name                 Destinations              Fare               Time\n");
-    printf("606910          Puri Express                Puri                    50             08:00 to 10:00 (58 km)\n");
-    printf("606911          Brahmapur Lines             Brahmapur               150            09:00 to 00:30 (164 km)\n");
-    printf("606912          Cuttack Linear              Cuttack                 30             13:00 to 13:50 (34 km)\n");
-    printf("606913          Balasore Express            Balasore                200            06:00 to 10:50 (207 km)\n");
-    printf("606914          Rourkela Lines              Rourkela                500            22:00 to 06:00 (327 km)\n");
-    printf("606915          Sambalpur Linear            Sambalpur               450            21:00 to 04:30 (319 km)\n");
-    printf("605822          Kolkata Linear              Kolkata                 600            06:00 to 16:00 (450 km)\n");
-    printf("604049          Hyderabad Express           Hyderabad               1200           16:00 to 12:00 (1024 km)\n");
-    printf("605157          Ranchi Lines                Ranchi                  600            19:00 to 06:00 (506 km)\n");
-    printf("607388          Nagpur Express              Nagpur                  1000           05:00 to 22:30 (840 km)\n\n");
+    printf("Bus No.          Bus Name                 Destinations              Fare               Time\n");
+    printf("606910          Puri Express                Puri                    50             08:00 to 10:00 (58 km)\n");
+    printf("606911          Brahmapur Lines             Brahmapur               150            09:00 to 00:30 (164 km)\n");
+    printf("606912          Cuttack Linear              Cuttack                 30             13:00 to 13:50 (34 km)\n");
+    printf("606913          Balasore Express            Balasore                200            06:00 to 10:50 (207 km)\n");
+    printf("606914          Rourkela Lines              Rourkela                500            22:00 to 06:00 (327 km)\n");
+    printf("606915          Sambalpur Linear            Sambalpur               450            21:00 to 04:30 (319 km)\n");
+    printf("605822          Kolkata Linear              Kolkata                 600            06:00 to 16:00 (450 km)\n");
+    printf("604049          Hyderabad Express           Hyderabad               1200           16:00 to 12:00 (1024 km)\n");
+    printf("605157          Ranchi Lines                Ranchi                  600            19:00 to 06:00 (506 km)\n");
+    printf("607388          Nagpur Express              Nagpur                  1000           05:00 to 22:30 (840 km)\n\n");
     printf("Enter any key to go back to Main Menu...\n\n");
 
 }
